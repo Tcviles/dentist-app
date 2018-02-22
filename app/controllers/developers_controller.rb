ca83045e35bec85a91b47225881250bb802faf7a
Thomas Viles <thomas.c.viles.jr@gmail.com>
2018-02-22 06:07:30 +0000

    added delete and edit to dentists

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -20,13 +20,38 @@ class DevelopersController < ApplicationController
     end
   end
 
-  post '/dentists/edit' do
+  post'/dentists' do
+    if !!params[:edit]
+      @dentist = Dentist.find_by_id(params[:edit])
+      redirect to "/dentists/#{@dentist.id}/edit"
+    elsif !!params[:delete]
+      @dentist = Dentist.find_by_id(params[:delete])
+      redirect to "/dentists/#{@dentist.id}/delete"
+    else
+      redirect to '/dentists'
+    end
+  end
+
+  patch '/dentists/:id' do
+    @dentist = Dentist.find_by_id(params[:id])
+    @dentist.update(:name => params[:name], :fee => params[:fee])
+    redirect to '/dentists'
+  end
+
+  get '/dentists/:id/edit' do
     if !!session[:developer?]
-      binding.pry
-      @dentist = Dentist.find_by
-      erb :'dentist/update'
+      @dentist = Dentist.find_by_id(params[:id])
+      erb :'dentists/update'
     else
       redirect to '/'
+    end
+  end
+
+  get '/dentists/:id/delete' do
+    if !!session[:developer?]
+      @dentist = Dentist.find_by_id(params[:id])
+      @dentist.delete
+      redirect to '/dentists'
     end
   end
 end
