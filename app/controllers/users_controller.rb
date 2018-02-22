c647e8d93c535e40919f74413cb2574bc3aed4b5
Thomas Viles <thomas.c.viles.jr@gmail.com>
2018-02-22 06:03:11 +0000

    Added delete and edit functionality to dentists

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
     if !logged_in?
       redirect to '/'
     else
-      @user = User.find_by(session[:user_id])
+      @user = User.find_by_id(session[:user_id])
+      binding.pry
       erb :'users/show'
     end
   end
@@ -23,21 +24,21 @@ class UsersController < ApplicationController
       redirect to '/signup'
     else
       @user = User.new(:name => params[:name], :email => params[:email], :password => params[:password])
-      if !params[:insurance][:name].empty?
+      if params[:insurance][:name] == ""
         @user.insurance = Insurance.create(company: params[:insurance][:name], coverage:1000)
       else
         @user.insurance = Insurance.find_by_id(params[:user][:insurance_id])
       end
 
-      if !params[:insurance][:name].empty?
+      if params[:insurance][:name] == ""
         @user.dentist = Dentist.create(name: params[:dentist][:name], fee:2000)
       else
         @user.dentist = Dentist.find_by_id(params[:user][:dentist_id])
       end
       @user.save
       session[:user_id] = @user.id
-      session[:developer?] = true if @user.name="Thomas"
-      redirect to '/user/:id'
+      session[:developer?] = true if @user.name=="Thomas"
+      redirect to "/user/#{session[:user_id]}"
     end
   end
 
@@ -45,7 +46,7 @@ class UsersController < ApplicationController
     if !logged_in?
       erb :'users/login'
     else
-      redirect '/user/:id'
+      redirect "/user/#{session[:user_id]}"
     end
   end
 
@@ -53,8 +54,8 @@ class UsersController < ApplicationController
     @user = User.find_by(:email => params[:email])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      session[:developer?] = true if @user.name = "Thomas"
-      redirect "/user/:id"
+      session[:developer?] = true if @user.name == "Thomas"
+      redirect "/user/#{session[:user_id]}"
     else
       redirect to '/signup'
     end
