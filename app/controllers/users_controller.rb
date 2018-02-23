8a2a2c13b07570bd8f9e93a531fd7d2151753128
Thomas Viles <thomas.c.viles.jr@gmail.com>
2018-02-23 04:52:46 +0000

    fixed user delete option

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  DEVELOPERS = ["Thomas"]
+  DEVELOPERS = [1]
 
   get '/user/:id' do
     if !logged_in?
@@ -48,7 +48,6 @@ class UsersController < ApplicationController
     else
       @user = User.new(:name => params[:name], :email => params[:email], :password => params[:password])
       if !params[:insurance][:name].empty?
-        binding.pry
         @user.insurance = Insurance.create(company: params[:insurance][:name], coverage:1000)
       else
         @user.insurance = Insurance.find_by_id(params[:user][:insurance_id])
@@ -58,11 +57,10 @@ class UsersController < ApplicationController
         @user.dentist = Dentist.create(name: params[:dentist][:name], fee:2000)
       else
         @user.dentist = Dentist.find_by_id(params[:user][:dentist_id])
-        binding.pry
       end
       @user.save
       session[:user_id] = @user.id
-      session[:developer?] = true if DEVELOPERS.include?(@user.name)
+      session[:developer?] = true if DEVELOPERS.include?(@user.id)
       redirect to "/user/#{session[:user_id]}"
     end
   end
@@ -87,7 +85,7 @@ class UsersController < ApplicationController
     @user = User.find_by(:email => params[:email])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      session[:developer?] = true if DEVELOPERS.include?(@user.name)
+      session[:developer?] = true if DEVELOPERS.include?(@user.id)
       redirect "/user/#{session[:user_id]}"
     else
       redirect to '/signup'
@@ -104,8 +102,8 @@ class UsersController < ApplicationController
   end
 
   get '/user/:id/delete' do
-    if session[:user_id] == params[:id]
-      @user = Uer.find_by_id(params[:id])
+    if session[:user_id] == params[:id].to_i
+      @user = User.find_by_id(params[:id])
       @user.delete
       redirect to '/'
     else
